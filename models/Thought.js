1f08386b94f09a19b60041f8e4e5580bafb3892d
Aster Oliva <asterthealabaster@gmail.com>
2024-06-13 02:41:14 -0700

    developed models/schemas for mongoose

---
+++ models/Thought.js
@@ -0,0 +1,58 @@
+const { Schema, model, Types } = require("mongoose");
+
+// Schema for Reaction subdocument
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: [true, "Reaction cannot be blank"],
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => new Date(createdAtVal).toLocaleString(),
+  },
+});
+
+// Schema for Thought document
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: [true, "Thought cannot be blank"],
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => new Date(createdAtVal).toLocaleString(),
+  },
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+  },
+  reactions: [reactionSchema],
+});
+
+// Virtual to get total count of reactions
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+// Set the schema toJSON options
+thoughtSchema.set("toJSON", {
+  virtuals: true,
+  getters: true,
+});
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require("mongoose");
+
+// Schema for User document
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    unique: true,
+    trimmed: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    trimmed: true,
+    match: [
+      /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/,
+      "Email address entered is not valid",
+    ],
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+// Virtual to get total count of user's friends
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// Virtual to get total count of user's thoughts
+userSchema.virtual("thoughtCount").get(function () {
+  return this.thoughts.length;
+});
+
+// Set the schema toJSON options
+userSchema.set("toJSON", {
+  virtuals: true,
+  getters: true,
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
