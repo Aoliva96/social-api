1910023fcb66f6abd02bbdc4ac59b523f65bc8e1
Aster Oliva <asterthealabaster@gmail.com>
2024-06-13 09:39:33 -0700

    attempted bugfix for data seeding, not yet working

--- utils/data.js
+++ utils/data.js
@@ -4,182 +4,332 @@ const users = [
     username: "EmilyDavis",
     email: "emilydavis@example.com",
     thoughts: [],
-    friends: [],
+    friends: ["SarahJohnson", "MichaelBrown"],
   },
   {
     username: "SarahJohnson",
     email: "sarahjohnson@example.com",
     thoughts: [],
-    friends: [],
+    friends: ["EmilyDavis", "MichaelBrown"],
   },
   {
     username: "MichaelBrown",
     email: "michaelbrown@example.com",
     thoughts: [],
-    friends: [],
+    friends: ["EmilyDavis", "SarahJohnson"],
   },
   {
     username: "JessicaMiller",
     email: "jessicamiller@example.com",
     thoughts: [],
-    friends: [],
+    friends: ["DavidWilson"],
   },
   {
     username: "DavidWilson",
     email: "davidwilson@example.com",
     thoughts: [],
-    friends: [],
+    friends: ["JessicaMiller"],
   },
 ];
 
 // Thought seed data
 const thoughts = [
   {
-    thoughtText: "Enjoying a cup of coffee while coding.",
-    username: "",
-    reactions: [],
+    thoughtText: "Enjoying a cup of coffee while coding is the best.",
+    username: "EmilyDavis",
+    reactions: [
+      {
+        reactionBody: "Definitely!",
+        username: "SarahJohnson",
+      },
+      {
+        reactionBody: "I agree.",
+        username: "MichaelBrown",
+      },
+    ],
   },
   {
     thoughtText: "I love coding!",
-    username: "",
-    reactions: [],
+    username: "EmilyDavis",
+    reactions: [
+      {
+        reactionBody: "Same here!",
+        username: "JessicaMiller",
+      },
+    ],
   },
   {
     thoughtText: "Today is a beautiful day!",
-    username: "",
-    reactions: [],
+    username: "SarahJohnson",
+    reactions: [
+      {
+        reactionBody: "Nice!",
+        username: "EmilyDavis",
+      },
+    ],
   },
   {
     thoughtText: "Feeling motivated!",
-    username: "",
-    reactions: [],
+    username: "SarahJohnson",
+    reactions: [
+      {
+        reactionBody: "That's great!",
+        username: "MichaelBrown",
+      },
+    ],
   },
   {
     thoughtText: "Just finished a challenging project.",
-    username: "",
-    reactions: [],
+    username: "MichaelBrown",
+    reactions: [
+      {
+        reactionBody: "Keep up the great work!",
+        username: "EmilyDavis",
+      },
+    ],
   },
   {
     thoughtText: "Excited for the weekend!",
-    username: "",
-    reactions: [],
+    username: "MichaelBrown",
+    reactions: [
+      {
+        reactionBody: "Understandable.",
+        username: "SarahJohnson",
+      },
+    ],
   },
   {
     thoughtText: "Learning something new every day!",
-    username: "",
-    reactions: [],
+    username: "JessicaMiller",
+    reactions: [
+      {
+        reactionBody: "Same here.",
+        username: "DavidWilson",
+      },
+    ],
   },
   {
     thoughtText: "Grateful for my friends and family today.",
-    username: "",
-    reactions: [],
+    username: "JessicaMiller",
+    reactions: [
+      {
+        reactionBody: "Me too.",
+        username: "DavidWilson",
+      },
+    ],
   },
   {
     thoughtText: "Working on a cool side project.",
-    username: "",
-    reactions: [],
+    username: "DavidWilson",
+    reactions: [
+      {
+        reactionBody: "Nice!",
+        username: "JessicaMiller",
+      },
+      {
+        reactionBody: "Can't wait to see the final product!",
+        username: "EmilyDavis",
+      },
+    ],
+  },
+  {
+    thoughtText: "Make sure to take a break and stretch every hour!",
+    username: "SarahJohnson",
+    reactions: [
+      {
+        reactionBody: "I should try this.",
+        username: "EmilyDavis",
+      },
+      {
+        reactionBody: "Good advice!",
+        username: "MichaelBrown",
+      },
+    ],
   },
 ];
 
-// Reaction seed data
-const reactions = [
-  {
-    reactionBody: "That's awesome!",
-    username: "",
-  },
-  {
-    reactionBody: "I agree.",
-    username: "",
-  },
-  {
-    reactionBody: "Cool!",
-    username: "",
-  },
-  {
-    reactionBody: "Nice!",
-    username: "",
-  },
-  {
-    reactionBody: "That's great!",
-    username: "",
-  },
-  {
-    reactionBody: "Very interesting.",
-    username: "",
-  },
-  {
-    reactionBody: "Understandable.",
-    username: "",
-  },
-  {
-    reactionBody: "Good job!",
-    username: "",
-  },
-  {
-    reactionBody: "Keep it up!",
-    username: "",
-  },
-];
-
-// Function to get random item from specified array
-const getRandomItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
-
-// Function to get 2 random items from specified array
-const getTwoRandomItems = (arr) =>
-  arr.sort(() => 0.5 - Math.random()).slice(0, 2);
-
-// Function to assign 2 random friends to each user without duplicates
-const assignFriends = () => {
-  let availableUsers = [...users];
-  for (let i = 0; i < users.length; i++) {
-    let randomFriends = getTwoRandomItems(availableUsers);
-    for (let j = 0; j < randomFriends.length; j++) {
-      if (randomFriends[j].username !== users[i].username) {
-        users[i].friends.push(randomFriends[j].username);
-      }
-    }
-    // Remove the assigned friends to avoid duplicates
-    availableUsers = availableUsers.filter(
-      (user) => user !== randomFriends[0] && user !== randomFriends[1]
-    );
-  }
+module.exports = {
+  users,
+  thoughts,
 };
 
-// Function to assign 2 random thoughts to users without duplicates
-const assignThoughts = () => {
-  let availableThoughts = [...thoughts];
-  for (let i = 0; i < users.length; i++) {
-    let randomThoughts = [];
-    for (let j = 0; j < 2; j++) {
-      let randomThought = getRandomItem(availableThoughts);
-      randomThought.username = users[i].username;
-      users[i].thoughts.push(randomThought);
-      randomThoughts.push(randomThought);
+// const users = [
+//   {
+//     username: "EmilyDavis",
+//     email: "emilydavis@example.com",
+//     thoughts: [],
+//     friends: [],
+//   },
+//   {
+//     username: "SarahJohnson",
+//     email: "sarahjohnson@example.com",
+//     thoughts: [],
+//     friends: [],
+//   },
+//   {
+//     username: "MichaelBrown",
+//     email: "michaelbrown@example.com",
+//     thoughts: [],
+//     friends: [],
+//   },
+//   {
+//     username: "JessicaMiller",
+//     email: "jessicamiller@example.com",
+//     thoughts: [],
+//     friends: [],
+//   },
+//   {
+//     username: "DavidWilson",
+//     email: "davidwilson@example.com",
+//     thoughts: [],
+//     friends: [],
+//   },
+// ];
 
-      // Remove the assigned thought to avoid duplicates
-      availableThoughts = availableThoughts.filter(
-        (thought) => thought !== randomThought
-      );
-    }
-  }
-};
+// // Thought seed data
+// const thoughts = [
+//   {
+//     thoughtText: "Enjoying a cup of coffee while coding.",
+//     username: "",
+//     reactions: [],
+//   },
+//   {
+//     thoughtText: "I love coding!",
+//     username: "",
+//     reactions: [],
+//   },
+//   {
+//     thoughtText: "Today is a beautiful day!",
+//     username: "",
+//     reactions: [],
+//   },
+//   {
+//     thoughtText: "Feeling motivated!",
+//     username: "",
+//     reactions: [],
+//   },
+//   {
+//     thoughtText: "Just finished a challenging project.",
+//     username: "",
+//     reactions: [],
+//   },
+//   {
+//     thoughtText: "Excited for the weekend!",
+//     username: "",
+//     reactions: [],
+//   },
+//   {
+//     thoughtText: "Learning something new every day!",
+//     username: "",
+//     reactions: [],
+//   },
+//   {
+//     thoughtText: "Grateful for my friends and family today.",
+//     username: "",
+//     reactions: [],
+//   },
+//   {
+//     thoughtText: "Working on a cool side project.",
+//     username: "",
+//     reactions: [],
+//   },
+// ];
 
-// Function to assign 1 random reaction to thoughts without duplicates
-const assignReactions = () => {
-  let availableReactions = [...reactions];
-  for (let i = 0; i < users.length; i++) {
-    for (let j = 0; j < users[i].thoughts.length; j++) {
-      let randomReaction = getRandomItem(availableReactions);
-      randomReaction.username = users[i].username;
-      users[i].thoughts[j].reactions.push(randomReaction);
+// // Reaction seed data
+// const reactions = [
+//   {
+//     reactionBody: "That's awesome!",
+//     username: "",
+//   },
+//   {
+//     reactionBody: "I agree.",
+//     username: "",
+//   },
+//   {
+//     reactionBody: "Cool!",
+//     username: "",
+//   },
+//   {
+//     reactionBody: "Nice!",
+//     username: "",
+//   },
+//   {
+//     reactionBody: "That's great!",
+//     username: "",
+//   },
+//   {
+//     reactionBody: "Very interesting.",
+//     username: "",
+//   },
+//   {
+//     reactionBody: "Understandable.",
+//     username: "",
+//   },
+//   {
+//     reactionBody: "Good job!",
+//     username: "",
+//   },
+//   {
+//     reactionBody: "Keep it up!",
+//     username: "",
+//   },
+// ];
 
-      // Remove the assigned reaction to avoid duplicates
-      availableReactions = availableReactions.filter(
-        (reaction) => reaction !== randomReaction
-      );
-    }
-  }
-};
+// // Function to get random item from specified array
+// const getRandomItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-// Export functions for use in seed.js
-module.exports = { assignFriends, assignThoughts, assignReactions };
+// // Function to get 2 random items from specified array
+// const getTwoRandomItems = (arr) =>
+//   arr.sort(() => 0.5 - Math.random()).slice(0, 2);
+
+// // Function to assign 2 random friends to each user
+// const assignFriends = () => {
+//   for (let i = 0; i < users.length; i++) {
+//     let randomFriends = getTwoRandomItems(users);
+//     for (let j = 0; j < randomFriends.length; j++) {
+//       if (randomFriends[j].username !== users[i].username) {
+//         users[i].friends.push(randomFriends[j].username);
+//       }
+//     }
+//   }
+// };
+
+// // Function to assign 2 random thoughts to each user
+// const assignThoughts = () => {
+//   for (let i = 0; i < users.length; i++) {
+//     let randomThoughts = [];
+//     for (let j = 0; j < 2; j++) {
+//       let randomThought = getRandomItem(thoughts);
+//       randomThought.username = users[i].username;
+//       users[i].thoughts.push(randomThought);
+//       randomThoughts.push(randomThought);
+//     }
+//   }
+// };
+
+// // Function to assign 1 random reaction to each thought
+// const assignReactions = () => {
+//   for (let i = 0; i < users.length; i++) {
+//     for (let j = 0; j < users[i].thoughts.length; j++) {
+//       let randomReaction = getRandomItem(reactions);
+//       randomReaction.username = users[i].username;
+//       users[i].thoughts[j].reactions.push(randomReaction);
+//     }
+//   }
+// };
+
+// // Function to assign friends and thoughts to users, assign reactions to thoughts, and return updated users and thoughts arrays
+// function generateData(users, thoughts) {
+//   assignFriends(users);
+//   assignThoughts(users, thoughts);
+//   assignReactions(users, thoughts);
+//   return { users, thoughts };
+// }
+
+// // Export functions for use in seed.js
+// module.exports = {
+//   generateData,
+//   assignFriends,
+//   assignThoughts,
+//   assignReactions,
+// };
--- utils/seed.js
+++ utils/seed.js
@@ -1,49 +1,40 @@
 const connection = require("../config/connection");
 const { User, Thought } = require("../models");
-const {
-  users,
-  thoughts,
-  assignFriends,
-  assignThoughts,
-  assignReactions,
-} = require("./data");
+const { users, thoughts } = require("./data");
 
-connection.on("error", console.error.bind(console, "connection error:"));
+connection.on("error", (err) => err);
 
 connection.once("open", async () => {
   console.log("Connected to database");
   try {
     // Clear the database of existing data
+    console.log("Attempting to clear database...");
     let checkForUsers = await connection.db
       .listCollections({ name: "users" })
       .toArray();
     if (checkForUsers.length) {
       await connection.db.dropCollection("users");
+      console.log("Users collection dropped");
     }
     let checkForThoughts = await connection.db
       .listCollections({ name: "thoughts" })
       .toArray();
     if (checkForThoughts.length) {
       await connection.db.dropCollection("thoughts");
+      console.log("Thoughts collection dropped");
     }
 
-    // Assign random friends, thoughts, and reactions
-    assignFriends(users);
-    assignThoughts(users, thoughts);
-    assignReactions(users, thoughts);
+    // Seed database with new data
+    console.log("Attempting to seed database...");
+    const newUsers = await User.insertMany(users);
+    console.log("Users seeded successfully");
+    console.table(newUsers);
 
-    // Seed database with updated data
-    for (let user of users) {
-      const newUser = await User.create(user);
-      console.log(`User ${newUser.username} created`);
-      for (let thought of user.thoughts) {
-        thought.userId = newUser._id;
-        await Thought.create(thought);
-        console.log(`Thought created for ${newUser.username}`);
-      }
-    }
+    const newThoughts = await Thought.insertMany(thoughts);
+    console.log("Thoughts seeded successfully");
+    console.table(newThoughts);
 
-    console.log("All data seeded successfully");
+    console.log("All data seeded successfully!");
     process.exit(0);
   } catch (error) {
     console.error(error);
